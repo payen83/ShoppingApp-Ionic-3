7b8b85a9d37dee5f3788debc16cf17ceb0719b71
Ferdy Fauzi <payen83@gmail.com>
2017-09-19 18:20:38 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ProductsPage } from '../pages/products/products';
+import { CartTabDefaultPagePage } from '../pages/cart-tab-default-page/cart-tab-default-page';
+import { CloudTabDefaultPagePage } from '../pages/cloud-tab-default-page/cloud-tab-default-page';
+import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
+import { ProductDetailsPage } from '../pages/product-details/product-details';
+import { AddNewPage } from '../pages/add-new/add-new';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    ProductsPage,
+    CartTabDefaultPagePage,
+    CloudTabDefaultPagePage,
+    TabsControllerPage,
+    ProductDetailsPage,
+    AddNewPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +30,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    ProductsPage,
+    CartTabDefaultPagePage,
+    CloudTabDefaultPagePage,
+    TabsControllerPage,
+    ProductDetailsPage,
+    AddNewPage
   ],
   providers: [
     StatusBar,
